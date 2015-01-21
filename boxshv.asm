de23b4f921e2fbb7f22f616d8cbba230384207b5
Adam Smith <unfactorable.polynomial@gmail.com>
2015-01-21 16:31:25 +0100

    initial commit

---
+++ boxshv.asm
@@ -0,0 +1,46 @@
+		BITS	16
+		ORG	0x100				; DOS loads us here
+Start:		; backup old KB interrupt
+		XOR	AX, AX
+		MOV	ES, AX				; ES = 0
+		MOV	AX, [ES:0x24]
+		MOV	[OldKBHandler], AX
+		MOV	AX, [ES:0x26]
+		MOV	[OldKBHandler + 2], AX
+;		; install new KB interrupt
+		MOV	WORD [ES:0x24], KBHandler
+		MOV	WORD [ES:0x26], CS
+		; set video mode 0x13
+		MOV	AX, 0x13
+		INT	0x10
+		; make ES point to the VGA memory
+		MOV	AX, 0xA000
+		MOV	ES, AX
+.gameLoop:	; check for exit
+		TEST	BYTE [Keys], 1			; 0 if esc not pressed
+		JZ	.gameLoop
+		; restore text mode 0x03
+		MOV	AX, 0x03
+		INT	0x10
+		; restore old KB interrupt
+		XOR	AX, AX
+		MOV	ES, AX
+		MOV	AX, [OldKBHandler]
+		MOV	[ES:0x24], AX
+		MOV	AX, [OldKBHandler + 2]
+		MOV	[ES:0x26], AX
+		; exit
+		MOV	AX, 0x4C00			; return code 0
+		INT	0x21
+
+KBHandler:	PUSH	AX
+		IN	AL, 0x60			; get key event
+		MOV	[Keys], AL			; copy to keys
+		MOV	AL, 0x20			; ACK
+		OUT	0x20, AL			; send ACK
+		POP	AX
+		IRET
+
+OldKBHandler:	DD	0
+
+Keys:		DB	0
