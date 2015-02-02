c7c0dd47a4424f9b7dad44a99c9aa1f9548f4c48
Adam Smith <unfactorable.polynomial@gmail.com>
2015-02-02 01:58:59 +0100

    split out box logic

---
+++ boxes.asm
@@ -0,0 +1,18 @@
+; CX = box to find
+; sets E if the box was found
+; returns a pointer in AX if the box is found
+FindBox:	PUSH	ES
+		PUSH	DI
+		MOV	AX, DS
+		MOV	ES, AX
+		MOV	AX, CX
+		MOV	CX, [BoxCnt]
+		; iterate through until we find a matching box or run out of
+		; boxes
+		MOV	DI, Boxes
+		REPNE	SCASW
+		MOV	CX, AX				; restore CX
+		MOV	AX, DI				; return pointer in AX
+		POP	DI
+		POP	ES
+		RET
--- boxshv.asm
+++ boxshv.asm
@@ -37,25 +37,6 @@ Start:		; backup old KB interrupt
 		MOV	AX, 0x4C00			; return code 0
 		INT	0x21
 
-; CX = box to find
-; sets E if the box was found
-; returns a pointer in AX if the box is found
-FindBox:	PUSH	ES
-		PUSH	DI
-		MOV	AX, DS
-		MOV	ES, AX
-		MOV	AX, CX
-		MOV	CX, [BoxCnt]
-		; iterate through until we find a matching box or run out of
-		; boxes
-		MOV	DI, Boxes
-		REPNE	SCASW
-		MOV	CX, AX				; restore CX
-		MOV	AX, DI				; return pointer in AX
-		POP	DI
-		POP	ES
-		RET
-
 
 ScrBase:	DW	0
 
@@ -94,6 +75,7 @@ OldKBHandler:	DD	0
 Quit:		DB	0
 
 %include "board.asm"
+%include "boxes.asm"
 %include "draw.asm"
 %include "kb.asm"
 %include "player.asm"
